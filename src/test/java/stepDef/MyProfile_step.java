b41fc94b35a6c773caf2b21022cf83ad28ef063e
Shaon321 <lumaknun@gmail.com>
2021-10-11 22:16:56 -0700

    Practice1

---
+++ src/test/java/stepDef/MyProfile_step.java
@@ -0,0 +1,9 @@
+package stepDef;
+
+import io.cucumber.java.en.Then;
+
+public class MyProfile_step {
+    @Then("I should be able to successfully login")
+    public void iShouldBeAbleToSuccessfullyLogin() {
+    }
+}
